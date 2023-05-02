4f25e70e343132adbcde9691af607fe1115f9cab
manal7h <manal.alsenani22@gmail.com>
2023-05-02 03:39:49 -0800

    repositories for each entity in Service

--- com.bankSystem.codeline/com.bankSystem.codeline/src/main/java/Services/CreditCardServices.java
+++ com.bankSystem.codeline/com.bankSystem.codeline/src/main/java/Services/CreditCardServices.java
@@ -1,4 +1,13 @@
 package Services;
 
+import Repositories.CreditCardRepositories;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+// This is where the business logic is done and everything related to the function and others and To give controller whatever it needs
 public class CreditCardServices {
+
+    @Autowired // create one instance and can be used in the entire program , no need to create obj
+    CreditCardRepositories creditCardRepositories;
 }
--- com.bankSystem.codeline/com.bankSystem.codeline/src/main/java/Services/LoanServices.java
+++ com.bankSystem.codeline/com.bankSystem.codeline/src/main/java/Services/LoanServices.java
@@ -1,4 +1,14 @@
 package Services;
 
+import Repositories.LoanRepositories;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+// This is where the business logic is done and everything related to the function and others and To give controller whatever it needs
 public class LoanServices {
+
+    @Autowired // create one instance and can be used in the entire program , no need to create obj
+    LoanRepositories loanRepositories;
+
 }
